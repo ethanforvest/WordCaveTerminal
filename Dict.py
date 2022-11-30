6f7f15c5b3411cbe8a23d0e0c6e7da3e59e11ab7
ethanforvest <ethanforvest@gmail.com>
2022-11-30 15:00:09 +0530

    Maximized the Count of Definitions and Examples

--- Dict.py
+++ Dict.py
@@ -1,27 +1,20 @@
 import requests
 import json
 from colorama import init
-init(autoreset=True)
 from colorama import Fore, Back, Style
 
+init(autoreset=True)
 word = input("Enter a word: ")
 URL = "https://define.wrdp.app/" + word
 
 r = requests.get(URL)
 json = r.json()
 
-definition_1 = json['Senses'][0]['Definition']
-Examples_1 = json['Senses'][0]['Examples']
-
-definition_2 = json['Senses'][1]['Definition']
-Examples_2 = json['Senses'][1]['Examples']
-
-print("\n")
-print(f"Meaning: {Fore.GREEN + definition_1}")
-print(f"Examples: {Examples_1}\n\n")
-
-print(f"Meaning: {Fore.GREEN + definition_2}")
-print(f"Examples: {Examples_2}")
-
-
-	
+for word in range(5):
+	try:
+  		definition = json['Senses'][word]['Definition']
+  		examples = json['Senses'][word]['Examples']
+	except:
+  		break
+	print(f"Meaning: {Fore.GREEN + definition}")
+	print(f"Example: {examples}")
