aa3f9a8b0647a13b1ed42845a2411a386b700b8f
ethanforvest <ethanforvest@gmail.com>
2022-11-30 13:54:21 +0530

    Colored Texts

--- Dict.py
+++ Dict.py
@@ -1,5 +1,8 @@
 import requests
 import json
+from colorama import init
+init(autoreset=True)
+from colorama import Fore, Back, Style
 
 word = input("Enter a word: ")
 URL = "https://define.wrdp.app/" + word
@@ -14,10 +17,10 @@ definition_2 = json['Senses'][1]['Definition']
 Examples_2 = json['Senses'][1]['Examples']
 
 print("\n")
-print(f"Meaning: {definition_1}")
+print(f"Meaning: {Fore.GREEN + definition_1}")
 print(f"Examples: {Examples_1}\n\n")
 
-print(f"Meaning: {definition_2}")
+print(f"Meaning: {Fore.GREEN + definition_2}")
 print(f"Examples: {Examples_2}")
 
 
