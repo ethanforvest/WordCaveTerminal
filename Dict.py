d0b1dde4ae801f35e38dcf1e757123e3378b8524
ethanforvest <ethanforvest@gmail.com>
2022-12-01 12:08:32 +0530

    Endless Answer

--- Dict.py
+++ Dict.py
@@ -3,18 +3,20 @@ import json
 from colorama import init
 from colorama import Fore, Back, Style
 
-init(autoreset=True)
-word = input("Enter a word: ")
-URL = "https://define.wrdp.app/" + word
+while True:
+	init(autoreset=True)
+	word = input("Enter a word: ")
+	URL = "https://define.wrdp.app/" + word
 
-r = requests.get(URL)
-json = r.json()
+	r = requests.get(URL)
+	json = r.json()
 
-for word in range(15):
-	try:
-  		definition = json['Senses'][word]['Definition']
-  		examples = json['Senses'][word]['Examples']
-	except:
-  		break
-	print(f"Meaning: {Fore.GREEN + definition}")
-	print(f"Example: {examples}")
+	for word in range(15):
+		try:
+	  		definition = json['Senses'][word]['Definition']
+	  		examples = json['Senses'][word]['Examples']
+		except:
+	  		break
+		print(f"Meaning: {Fore.GREEN + definition}")
+		print(f"Example: {examples}")
+	print("\n")
