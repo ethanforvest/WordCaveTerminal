6e2e46cb26bd3396c9d45ba289ec38b17a4b5558
ethanforvest <ethanforvest@gmail.com>
2022-12-04 22:55:31 +0530

    IMGs/GIFs support

--- Wordup-Terminal/Extractors/wordup.py
+++ Wordup-Terminal/Extractors/wordup.py
@@ -1,16 +1,17 @@
-def WordText(json):  # Resulted word
+def WordText(self):  # Resulted word
     try:
-        WordText_ = json['WordText']
+        WordText_ = self['WordText']
     except:
         print("Couldn't find the word :(")
-    return WordText_
+    else:
+        return WordText_
 
 
-def definition_counter(json):  # Looking for how many definitions are there
+def definition_counter(self):  # Looking for how many definitions are there
     definition_count = 0
     for word in range(100):
         try:
-            definition = json['Senses'][word]['Definition']
+            definition = self['Senses'][word]['Definition']
             definition_count = definition_count + 1
         except:
             break
@@ -31,26 +32,28 @@ def example_counter(json):  # Looking for how many examples are there
 def gifs(self):  # Looking for images and GIFs
     import requests
 
-    URL = f"https://word-images.cdn-wordup.com/opt/{WordText(self)}/selected.json"
+    URL = f"https://word-images.cdn-wordup.com/opt/{self}/selected.json"
     r = requests.get(URL)
     gifs = r.json()
-
-
-def id_finder(json):
-    id = json['Senses'][word]['ID']
+    return gifs
 
 
 def printer(json):  # Putting everything together and print them
     from colorama import init, Style, Fore
 
-    # Resetting the color back to the default color after the end of every print statement is executed
     init(autoreset=True)
     definition_count = definition_counter(json)
 
-    print(f"Enter a word: {WordText(json)}")
+    found_word = WordText(json)
+    print(f"Enter a word: {found_word}")
+
+    gif_json = gifs(found_word)   # All the avilable GIFS/IMGs
 
     for word in range(definition_count):
+
         definition = json['Senses'][word]['Definition']
+        i_d = json['Senses'][word]['ID']
+
         try:
             word_type = json['Senses'][word]['Type']
         except:
@@ -63,3 +66,12 @@ def printer(json):  # Putting everything together and print them
             continue
         else:
             print(f"Example: {examples}")
+        finally:
+            # looking for the correct GIF/IMG
+            for key, value in gif_json.items():
+                if key == i_d:
+                    if "https" not in value:
+                        print(
+                            f"{Fore.CYAN}GIF/IMG:{Style.RESET_ALL} https://word-images.cdn-wordup.com/opt/{value}")
+                    else:
+                        print(f"{Fore.CYAN}GIF/IMG:{Style.RESET_ALL} {value}")
